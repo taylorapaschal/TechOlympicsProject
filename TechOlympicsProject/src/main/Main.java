73101ae5ac2446a6104eada10e843895c11994b9
taylorapaschal <taylorapaschal@gmail.com>
2021-02-17 17:52:40 -0500

    Printed event name

---
+++ TechOlympicsProject/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String eventName = "TechOlympics";
+		
+		System.out.println(eventName);
+
+
+	}
+
+}
